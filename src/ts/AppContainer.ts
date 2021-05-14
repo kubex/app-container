c204a3537def837480d948fd52b0810c98d6c768
Tom Kay <oridan82@gmail.com>
2021-05-14 10:27:42 +0100

    move declaration to inline

--- src/ts/AppContainer.ts
+++ src/ts/AppContainer.ts
@@ -1,6 +1,7 @@
 import {html, LitElement, unsafeCSS} from 'lit';
 import {customElement} from 'lit/decorators.js';
-import styles from "../scss/container.scss";
+// @ts-ignore
+import styles from '../scss/container.scss';
 
 @customElement('app-container')
 export class AppContainer extends LitElement {
