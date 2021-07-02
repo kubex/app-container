42bcd8f64a410b476d634a1a4eec926be5daef4b
Tom Kay <oridan82@gmail.com>
2021-07-02 15:34:52 +0100

    support loading scripts

--- src/ts/AppContainer.ts
+++ src/ts/AppContainer.ts
@@ -1,16 +1,53 @@
 import {html, LitElement, unsafeCSS} from 'lit';
-import {customElement} from 'lit/decorators.js';
+import {customElement, property} from 'lit/decorators.js';
 // @ts-ignore
 import styles from '../scss/container.scss';
 
 @customElement('app-container')
 export class AppContainer extends LitElement {
+    @property({type: Boolean, attribute: 'allow-scripts'})
+    private allowScripts: boolean = false;
+
     static get styles() {
         return unsafeCSS(styles);
     }
 
+    connectedCallback() {
+        super.connectedCallback();
+
+        // @ts-ignore
+        this.shadowRoot.createElement = function () {
+            return document.createElement.apply(document, [...arguments]);
+        }
+
+        // @ts-ignore
+        this.shadowRoot.body = this.shadowRoot;
+    }
+
     set innerHTML(data) {
         this.shadowRoot.innerHTML = "<link href=\"https://fonts.googleapis.com/icon?family=Material+Icons+Outlined\" rel=\"stylesheet\">" + data;
+
+        if (!this.allowScripts) {
+            return;
+        }
+
+        const scripts = this.shadowRoot.querySelectorAll('script');
+        scripts.forEach((script) => {
+            if (script.matches('[src]')) {
+                fetch(script.getAttribute('src'))
+                    .then(response => response.text())
+                    .then((body) => {
+                        this._evalCode(body)
+                    });
+            } else {
+                this._evalCode(script.innerHTML)
+            }
+        });
+    }
+
+    private _evalCode(content) {
+        const fn = new Function('document', content);
+        fn(this.shadowRoot);
     }
 
     render() {
